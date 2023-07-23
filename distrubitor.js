a41032702dee4850a110fc823ee50c621ff8c8e9
Mikkhall <mikkel@hallingstad.me>
2023-07-23 06:33:40 +0200

    .

---
+++ distrubitor.js
@@ -0,0 +1,46 @@
+module.exports = {
+    // a function to run the logic for this role
+    /** @param {Creep} creep */
+    run: function (creep) {
+        if (creep.memory.working == true && creep.store.getCapacity() == creep.store.getFreeCapacity()) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.store.getFreeCapacity() == 0) {
+            creep.memory.working = true;
+        }
+
+        if (creep.store.getFreeCapacity() == creep.store.getCapacity() && creep.ticksToLive <= 50) {
+            creep.suicide();
+        }
+
+        if (creep.memory.working == true) {
+            let structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                filter: (s) =>  (s.structureType == STRUCTURE_STORAGE)
+            });
+
+            if (structure != undefined) {
+                for(const resourceType in creep.carry) {
+                    if (creep.transfer(structure, resourceType) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structure);
+                    }
+                }
+            }
+            else {
+                creep.memory.working = false;
+            }
+        }
+
+        else {
+            let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: s => s.structureType == STRUCTURE_TERMINAL
+            });
+            if (container != undefined) {
+                for(let resourceType in container.store) {
+                    if (creep.withdraw(container, resourceType) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container);
+                    }
+                }
+            }
+        }
+    }
+};
